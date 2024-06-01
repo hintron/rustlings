aac0bd637afc86bfb2bc696d6486fe7acdd64bcf
Michael Hinton <github@hinton.e4ward.com>
2024-06-01 13:52:01 -0600

    Do strings

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,31 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+
+    // // Method 1
+    // let mut input = input.to_string();
+    // input.push_str(" world!");
+    // input
+    // // Method 2
+    // input.to_string() + " world!"
+    // Method 3
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let word = "cars";
+    match input.find("cars") {
+        Some(index) => input[0..index].to_string() + "balloons" + &input[index + word.len()..],
+        None => input.to_string()
+    }
 }
 
 #[cfg(test)]
